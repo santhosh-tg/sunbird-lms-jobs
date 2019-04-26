8928560b86e98ede67322df4352288b04a259ab9
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2019-04-26 19:36:38 +0530

    Update MessageValidatorTest.java

--- samza/jobs-common/src/test/java/org/sunbird/jobs/commom/test/MessageValidatorTest.java
+++ samza/jobs-common/src/test/java/org/sunbird/jobs/commom/test/MessageValidatorTest.java
@@ -12,30 +12,35 @@ import org.sunbird.validator.MessageValidator;
 import org.junit.Assert;
 
 public class MessageValidatorTest {
+
   @Test
-  public void testMessageValidationSuccess() {
+  public void testValidateMessageSuccess() {
     Map<String, Object> messageMap = createMessageMap();
     ProjectCommonException error = null;
     MessageValidator validator = new MessageValidator();
+
     try {
       validator.validateMessage(messageMap);
     } catch (ProjectCommonException e) {
       error = e;
     }
+    
     Assert.assertTrue(error == null);
   }
 
   @Test
-  public void testMessageValidationFailureWithoutObjectType() {
+  public void testValidateMessageFailureWithoutObjectType() {
     Map<String, Object> messageMap = createMessageMap();
     messageMap.remove(Constants.OBJECT_TYPE);
     ProjectCommonException error = null;
+
     MessageValidator validator = new MessageValidator();
     try {
       validator.validateMessage(messageMap);
     } catch (ProjectCommonException e) {
       error = e;
     }
+
     Assert.assertTrue(error.getCode().equals(ResponseCode.mandatoryParamsMissing.getErrorCode()));
   }
 
@@ -44,12 +49,14 @@ public class MessageValidatorTest {
     Map<String, Object> messageMap = createMessageMap();
     messageMap.remove(Constants.OPERATION_TYPE);
     ProjectCommonException error = null;
+
     MessageValidator validator = new MessageValidator();
     try {
       validator.validateMessage(messageMap);
     } catch (ProjectCommonException e) {
       error = e;
     }
+
     Assert.assertTrue(error.getCode().equals(ResponseCode.mandatoryParamsMissing.getErrorCode()));
   }
 
@@ -58,12 +65,14 @@ public class MessageValidatorTest {
     Map<String, Object> messageMap = createMessageMap();
     messageMap.remove(Constants.EVENT);
     ProjectCommonException error = null;
+
     MessageValidator validator = new MessageValidator();
     try {
       validator.validateMessage(messageMap);
     } catch (ProjectCommonException e) {
       error = e;
     }
+
     Assert.assertTrue(error.getCode().equals(ResponseCode.mandatoryParamsMissing.getErrorCode()));
   }
 
@@ -73,11 +82,13 @@ public class MessageValidatorTest {
     messageMap.remove(Constants.IDENTIFIER);
     ProjectCommonException error = null;
     MessageValidator validator = new MessageValidator();
+
     try {
       validator.validateMessage(messageMap);
     } catch (ProjectCommonException e) {
       error = e;
     }
+
     Assert.assertTrue(error.getCode().equals(ResponseCode.mandatoryParamsMissing.getErrorCode()));
   }
 
@@ -87,24 +98,29 @@ public class MessageValidatorTest {
     messageMap.remove(Constants.EVENT_TYPE);
     ProjectCommonException error = null;
     MessageValidator validator = new MessageValidator();
+
     try {
       validator.validateMessage(messageMap);
     } catch (ProjectCommonException e) {
       error = e;
     }
+
     Assert.assertTrue(error.getCode().equals(ResponseCode.mandatoryParamsMissing.getErrorCode()));
   }
 
   private Map<String, Object> createMessageMap() {
     Map<String, Object> messageMap = new HashMap<>();
+
     messageMap.put(Constants.IDENTIFIER, "123456");
     messageMap.put(Constants.OPERATION_TYPE, Constants.UPSERT);
     messageMap.put(Constants.EVENT_TYPE, Constants.TRANSACTIONAL);
     messageMap.put(Constants.OBJECT_TYPE, Constants.LOCATION);
+
     Map<String, Object> event = new HashMap<>();
     Map<String, Object> properties = new HashMap<>();
     Map<String, Object> name = new HashMap<>();
     Map<String, Object> id = new HashMap<>();
+    
     name.put(Constants.NV, "BLR");
     id.put(Constants.NV, "123456");
     properties.put("name", name);
@@ -114,7 +130,7 @@ public class MessageValidatorTest {
     messageMap.put(Constants.ETS, 123456L);
     messageMap.put(Constants.USER_ID, "ANONYMOUS");
     messageMap.put(Constants.CREATED_ON, "1556018741532");
+
     return messageMap;
   }
-
 }
