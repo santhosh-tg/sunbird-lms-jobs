4323c0e7c6d616f0b2a52b646a2aab3503ed5f14
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2019-05-06 16:21:52 +0530

    Update ResponseCode.java

--- samza/jobs-common/src/main/java/org/sunbird/jobs/samza/common/ResponseCode.java
+++ samza/jobs-common/src/main/java/org/sunbird/jobs/samza/common/ResponseCode.java
@@ -6,6 +6,7 @@ import org.sunbird.common.models.util.JsonKey;
 public enum ResponseCode {
     unAuthorized(ResponseMessage.Key.UNAUTHORIZED_USER, ResponseMessage.Message.UNAUTHORIZED_USER),
     invalidOrgData(ResponseMessage.Key.INVALID_ORG_DATA, ResponseMessage.Message.INVALID_ORG_DATA),
+
     OK(200),
     CLIENT_ERROR(400),
     SERVER_ERROR(500),
@@ -14,20 +15,11 @@ public enum ResponseCode {
     FORBIDDEN(403),
     REDIRECTION_REQUIRED(302),
     TOO_MANY_REQUESTS(429);
+
     private int responseCode;
-    /**
-     * error code contains String value
-     */
     private String errorCode;
-    /**
-     * errorMessage contains proper error message.
-     */
     private String errorMessage;
 
-    /**
-     * @param errorCode    String
-     * @param errorMessage String
-     */
     private ResponseCode(String errorCode, String errorMessage) {
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
@@ -39,48 +31,26 @@ public enum ResponseCode {
         this.responseCode = responseCode;
     }
 
-    /**
-     * @param errorCode
-     * @return
-     */
     public String getMessage(int errorCode) {
         return "";
     }
 
-    /**
-     * @return
-     */
     public String getErrorCode() {
         return errorCode;
     }
 
-    /**
-     * @param errorCode
-     */
     public void setErrorCode(String errorCode) {
         this.errorCode = errorCode;
     }
 
-    /**
-     * @return
-     */
     public String getErrorMessage() {
         return errorMessage;
     }
 
-    /**
-     * @param errorMessage
-     */
     public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
     }
 
-    /**
-     * This method will provide status message based on code
-     *
-     * @param code
-     * @return String
-     */
     public static String getResponseMessage(String code) {
         if (StringUtils.isBlank(code)) {
             return "";
@@ -106,13 +76,6 @@ public enum ResponseCode {
         this.responseCode = responseCode;
     }
 
-    /**
-     * This method will take header response code as int value and it provide matched enum value, if
-     * code is not matched or exception occurs then it will provide SERVER_ERROR
-     *
-     * @param code int
-     * @return HeaderResponseCode
-     */
     public static ResponseCode getHeaderResponseCode(int code) {
         if (code > 0) {
             try {
@@ -129,12 +92,6 @@ public enum ResponseCode {
         return ResponseCode.SERVER_ERROR;
     }
 
-    /**
-     * This method will provide ResponseCode enum based on error code
-     *
-     * @param errorCode
-     * @return String
-     */
     public static ResponseCode getResponse(String errorCode) {
         if (StringUtils.isBlank(errorCode)) {
             return null;
