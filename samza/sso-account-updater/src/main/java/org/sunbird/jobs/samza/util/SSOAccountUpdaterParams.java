394c14440c6cb73d90dffe24366e7ece821ee3a9
Hari-stackroute <40484996+Hari-stackroute@users.noreply.github.com>
2021-01-29 18:09:21 +0530

    SC-2168 removed  sso-account-updater and user-cert-merge modules (#53)
    * SC-2168 removed  sso-account-updater and user-cert-merge modules

--- samza/sso-account-updater/src/main/java/org/sunbird/jobs/samza/util/SSOAccountUpdaterParams.java
+++
@@ -1,7 +0,0 @@
-package org.sunbird.jobs.samza.util;
-
-public enum SSOAccountUpdaterParams {
-    event, userExternalId, nameFromPayload, channel, orgExternalId, roles, userId,
-    organisations, firstName, organisationId, lms_host, org_search_api, user_update_private_api,
-    externalId, filters, request, result, response, count, content, identifier, PUBLIC, es_host
-}
--- samza/user-cert-merge/src/main/java/org/sunbird/jobs/samza/util/UserAccountMergerParams.java
+++
@@ -1,5 +0,0 @@
-package org.sunbird.jobs.samza.util;
-
-public enum UserAccountMergerParams {
-    fromAccountId, toAccountId, lms_host, user_cert_merge_private_api, request, rootOrgId
-}
